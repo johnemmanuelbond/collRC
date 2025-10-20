38f6c6c2931dcb41be9598bad03539e1babec8a4
johnemmanuelbond <johnemmanuelbond@gmail.com>
2025-10-20 18:44:29 -0400

    cleanup

--- src/calc/__init__.py
+++ src/calc/__init__.py
@@ -4,6 +4,6 @@ from .locality import quat_to_angle, quat_to_angle, local_vectors
 
 from .bond_order import flat_bond_order, stretched_bond_order, projected_bond_order, crystal_connectivity
 
-from .p_atic import global_patic, local_patic
+from .orient_order import global_patic, local_patic
 
 from .clusters import graph_clusters, cluster_com
