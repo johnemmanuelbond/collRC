24ec9b34c10c9fa0a9f70b295e424b24b4c2bba4
johnemmuelbond <johnemmanuelbond@gmail.com>
2025-10-20 16:04:41 -0400

    trying to patch the readthedocs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@
 
 import os,sys
 sys.path.insert(0,os.path.abspath('..'))
-sys.path.insert(1,'../src')
+sys.path.insert(1,'../../src')
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
