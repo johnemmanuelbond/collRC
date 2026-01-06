ecca53ba3c0e393be93ef7a8c1fcb00e98f22243
mdemny1 <mdemny1@jhu.edu>
2026-01-06 15:16:29 -0500

    branch for chain patches

--- src/visuals/__init__.py
+++ src/visuals/__init__.py
@@ -2,3 +2,4 @@
 
 from .shapes import SuperEllipse, flat_patches, projected_patches, plot_principal_axes
 from .fields import Field, contour_PEL, spectral_PEL, PEL_arrows
+from .chains import chain_patches
---
+++ src/visuals/chains.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Docstring for chains
+"""
+
+import numpy as np
+
+def chain_patches(pts, view_dir = np.array([0,0,1]), view_ref = 'z'):
+    """
+    Docstring for chain_patches
+    
+    :param pts: Description
+    :param view_dir: Description
+    :param view_ref: Description
+    """
+
+    pass
+
+if __name__ == "__main__":
+
+    # for testing
+
+    pass
